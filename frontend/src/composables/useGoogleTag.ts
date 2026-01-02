c22f81abd081a37b976207e342997fc5cdd28964
Puzzler <rs@metadist.de>
2026-01-02 10:55:19 +0100

    formatting

--- frontend/src/composables/useGoogleTag.ts
+++ frontend/src/composables/useGoogleTag.ts
@@ -57,6 +57,7 @@ export const useGoogleTag = () => {
       // Script in head - use textContent and create text node to avoid XSS
       scriptElement = document.createElement('script')
       scriptElement.id = 'google-tag-manager'
+      // prettier-ignore
       const scriptText = document.createTextNode(
         `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${tagId}');`
       )
@@ -83,6 +84,7 @@ export const useGoogleTag = () => {
 
       // Initialize gtag - use textContent to avoid XSS
       const initScript = document.createElement('script')
+      // prettier-ignore
       const initText = document.createTextNode(
         `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${tagId}');`
       )
